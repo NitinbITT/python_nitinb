55b19df02530479017d0690a10c330597369cd08
NitinITT <nitinboopathy11@gmail.com>
2026-01-13 17:09:32 +0530

    Completed Day 2 Works and Learnings

---
+++ Learnings/Day-2/practice.py
@@ -0,0 +1,36 @@
+# from datetime import date
+# a = input("Enter your Name: ")
+# b=int(input("Enter Your DOB year: "))
+# current_year=date.today().year
+# print("Your Name is",a,"and your age is",(current_year-b))
+
+
+# a=int(input("Enter a Number: "))
+# if a%2==0:
+#     print("The Number is Even")
+# else:
+#     print("The Number is Odd")
+
+# a=10
+# s="Hello"
+# f=2.000000
+# c=5j
+# print(type(a))
+# print(type(s))
+# print(type(f))
+# print(type(c))
+
+# i=1
+# while i<=5:
+#     print(i,end=" ")
+#     i+=1
+
+
+# for i in range(1,11):
+#     if i==2:
+#         continue
+#     if i==8:
+#         break
+#     else:
+#         pass
+#     print(i,end=" ")
