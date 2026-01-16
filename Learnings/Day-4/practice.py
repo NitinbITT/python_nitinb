db1997bfbc0f49bd508757122d78d90db98fbb87
NitinITT <nitinboopathy11@gmail.com>
2026-01-16 17:15:06 +0530

    Completed Day-4 works and practices

---
+++ Learnings/Day-4/practice.py
@@ -0,0 +1,29 @@
+# def function():
+#     return "Hello World !!"
+# print(function())
+
+
+# def sum(a=1,b=2):
+#     return a+b
+# print(sum())
+
+# def function(name,age):
+#     print(name,age)
+
+# function(age=12,name="Alex")
+
+# def function(*args):
+#     for i in args:
+#         print(type(i))
+#         print(i)
+
+# function("a","b",1) 
+
+# sum= lambda a,b,c : a+b+c
+# print(sum(1,2,3))
+
+with open("file.txt","w") as file:
+    file.write("This is a line")
+    # for line in file:
+    #     print(line.strip())
+    # print(file)
